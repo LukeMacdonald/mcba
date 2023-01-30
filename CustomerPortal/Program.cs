0e8b78864a766c2648b514b401dedfe563eae859
Luke <lukemacdonald21@gmail.com>
2023-01-30 21:10:45 +1100

    Added all default files

--- CustomerPortal/Program.cs
+++ CustomerPortal/Program.cs
@@ -1,27 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// builder.Services.AddDbContext<MCBAContext>(options =>
+// {
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("McbaContext"));
+//     options.UseLazyLoadingProxies();
+//     
+// });
+builder.Services.AddDistributedSqlServerCache(options =>
+{
+    // options.ConnectionString = builder.Configuration.GetConnectionString(nameof(MCBAContext));
+    options.SchemaName = "dotnet";
+    options.TableName = "SessionCache";
+});
+builder.Services.AddSession(options => options.Cookie.IsEssential = true);
+
+// builder.Services.AddHostedService<BillPayService>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// Seed data.
+using(var scope = app.Services.CreateScope())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var services = scope.ServiceProvider;
+    try
+    {
+        // SeedData.Initialize(services);
+    }
+    catch(Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred seeding the DB.");
+    }
 }
 
-app.UseHttpsRedirection();
+// Configure the HTTP request pipeline.
+if(!app.Environment.IsDevelopment())
+    app.UseExceptionHandler("/Home/Error");
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
+app.UseSession(); // Enable session
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
 
 app.Run();
--- CustomerPortal/wwwroot/js/site.js
+++ CustomerPortal/wwwroot/js/site.js
@@ -1,4 +1,15 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Function to automatically add spacing to phone number form input
+document.getElementById('phone').addEventListener("keyup", function(event){
+    const key = event.key;
+    let txt = this.value;
 
-// Write your JavaScript code.
+    if ((txt.length===4 || txt.length===8) && key !=="Backspace")
+        this.value=this.value+" ";
+});
+// Function to automatically add spacing to TFN form input
+document.getElementById('tfn').addEventListener("keyup", function(event){
+    const key = event.key;
+    let txt = this.value;
+    if ((txt.length===3 || txt.length===7) && key !=="Backspace")
+        this.value=this.value+" ";
+});
