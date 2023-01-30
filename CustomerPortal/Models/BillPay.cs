d65cd22780607f8bc96d314a2e5c30b1a4f86c9d
Luke <lukemacdonald21@gmail.com>
2023-01-30 22:03:09 +1100

    Added BillPay and Payee Models

---
+++ CustomerPortal/Models/BillPay.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using A2Practice.Utilities;
+
+namespace CustomerPortal.Models;
+public class BillPay
+{
+    public int BillPayID { get; set; }
+    
+    [Required,ForeignKey(nameof(Account))]
+    public int AccountNumber { get; set; }
+    public virtual Account Account { get; set; }
+    
+    [Required,ForeignKey(nameof(Payee))]
+    public int PayID { get; set; }
+    public virtual Payee Payee { get; set; }
+    
+    [Required,Column(TypeName = "money")]
+    [IsPositive(ErrorMessage = "Only positive amounts allowed.")]
+    [DataType(DataType.Currency),TwoDecimalPlaces]
+    public decimal Amount { get; set; }
+    
+    [Required]
+    public DateTime ScheduleTimeUtc { get; set; }
+    
+    [Required]
+    public PeriodType Period { get; set; }
+    
+    // Variable indicating if the payment is still active
+    [Required]
+    public bool Active { get; set; }
+    
+    [Required]
+    public bool Failed { get; set; }
+}
